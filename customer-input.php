e874c43035c88d64e6863075b9ddb89ac2b8e177
Kawagoe <k09160908@yahoo.co.jp>
2025-03-31 12:45:43 +0900

    フォーム作成

---
+++ customer-input.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>donuts-site</title>
+  <link rel="stylesheet" href="customer.css">
+</head>
+<body>
+
+<h1>会員登録</h1>
+  <form action="customer-confirm">
+<ul>
+  <li>
+    <p>お名前<span>(必須)</span><input type="text" name="user_name1"></p>
+  </li>
+  <li>
+    <p>お名前 (フリガナ)<span>(必須)</span><input type="text" name="user_name2"></p>
+  </li>
+  <li>
+    <p>郵便番号<span>(必須)</span></p><input type="text" name="postcode">
+  </li>
+  <li>
+    <p>住所<span>(必須)</span></p><input type="text" name="address">
+  </li>
+  <li>
+    <p>メールアドレス<span>(必須)</span></p>
+    <input type="email" name="user_mail">
+  </li>
+  <li>
+    <p>パスワード<span>(必須)</span></p>
+    <p>A-Z、a-z、0-9を少なくとも各1つは含めて8文字以上で入力してください。</p>
+    <input type="password" name="password">
+</li>
+</ul>
+
+<input type="submit" value="ご入力内容を確認する">
+</form>
+
+</body>
+</html>
