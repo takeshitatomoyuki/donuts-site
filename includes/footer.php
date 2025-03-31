17a5dfa34bbbcd53557a55ec121c27916b414adf
8000-16000 <goto8303taizo@gmail.com>
2025-03-31 15:18:46 +0900

    フッター( ˘•ω•˘ )

---
+++ includes/footer.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../common/css/reset.css">
+  <link rel="stylesheet" href="../common/css/common.css">
+</head>
+<body>
+
+  <footer>
+    <ul>
+      <li class="nav_text">よくある質問</li>
+      <li class="nav_text">お問い合わせ</li>
+      <li class="nav_text">当サイトのポリシー</li>
+    </ul>
+    <ul>
+      <a href="">
+        <li><img src="../common/images/instagram.png" alt=""></li>
+      </a>
+      <a href="">
+        <li><img src="../common/images/facebook.png" alt=""></li>
+      </a>
+      <a href="">
+        <li><img src="../common/images/x.png" alt=""></li>
+      </a>
+    </ul>
+
+    <img src="../common/images/footer-logo.svg" alt="" class="footer_logo">
+    <p>Copyright (C) c.c.donuts All Rights Reserved</p>
+  </footer>
+  
+</body>
+</html>
