78de9781b6a3afb3b7f737c725ea7a2532f5c5a3
takeshitatomoyuki <takeshitaprogramming@gmail.com>
2025-04-01 14:59:16 +0900

    ヘッダー

--- logout-input.php
+++ logout-input.php
@@ -1,5 +1,5 @@
-<?php require '../header.php'; ?>
+<?php require 'header.php'; ?>
 <h1>ログアウト</h1>
 <p>ログアウトしますか？</p>
 <a href="logout-complete.php">ログアウトする</a>
-<?php require '../footer.php'; ?>
+<?php require 'footer.php'; ?>
