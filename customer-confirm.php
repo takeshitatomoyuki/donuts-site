9e3fd820c09c95b73ea7537d90bd18832adad9df
takeshitatomoyuki <takeshitaprogramming@gmail.com>
2025-04-01 10:43:43 +0900

    confirm

--- customer-confirm.php
+++ customer-confirm.php
@@ -1,11 +1,11 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // 入力データを取得
-    $user_name1 = htmlspecialchars($_POST["user_name1"], ENT_QUOTES, "UTF-8");
-    $user_name2 = htmlspecialchars($_POST["user_name2"], ENT_QUOTES, "UTF-8");
-    $postcode = htmlspecialchars($_POST["postcode"], ENT_QUOTES, "UTF-8");
+    $name = htmlspecialchars($_POST["name"], ENT_QUOTES, "UTF-8");
+    $kana = htmlspecialchars($_POST["kana"], ENT_QUOTES, "UTF-8");
+    $post_code = htmlspecialchars($_POST["post_code"], ENT_QUOTES, "UTF-8");
     $address = htmlspecialchars($_POST["address"], ENT_QUOTES, "UTF-8");
-    $user_mail = htmlspecialchars($_POST["user_mail"], ENT_QUOTES, "UTF-8");
+    $mail = htmlspecialchars($_POST["mail"], ENT_QUOTES, "UTF-8");
     $password = htmlspecialchars($_POST["password"], ENT_QUOTES, "UTF-8");
 
 } else {
@@ -25,33 +25,31 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <h2>入力内容の確認</h2>
     <p>以下の内容で登録しますか？</p>
     <form action="complete.php" method="post">
-        <p>お名前: <?php echo $user_name1; ?></p>
-        <p>お名前 (フリガナ): <?php echo $user_name2; ?></p>
-        <p>郵便番号: <?php echo $postcode; ?></p>
+        <p>お名前: <?php echo $name; ?></p>
+        <p>お名前 (フリガナ): <?php echo $kana; ?></p>
+        <p>郵便番号: <?php echo $post_code; ?></p>
         <p>住所: <?php echo $address; ?></p>
-        <p>メールアドレス: <?php echo $user_mail; ?></p>
+        <p>メールアドレス: <?php echo $mail; ?></p>
         <p>パスワード: <?php echo $password; ?></p>
 
         <!-- hidden フィールドでデータを complete.php に渡す -->
-        <input type="hidden" name="user_name1" value="<?php echo $user_name1; ?>">
-        <input type="hidden" name="user_name2" value="<?php echo $user_name2; ?>">
-        <input type="hidden" name="postcode" value="<?php echo $postcode; ?>">
+        <input type="hidden" name="name" value="<?php echo $name; ?>">
+        <input type="hidden" name="kana" value="<?php echo $kana; ?>">
+        <input type="hidden" name="post_code" value="<?php echo $post_code; ?>">
         <input type="hidden" name="address" value="<?php echo $address; ?>">
-        <input type="hidden" name="postcode" value="<?php echo $postcode; ?>">
-        <input type="hidden" name="postcode" value="<?php echo $user_mail; ?>">
-        <input type="hidden" name="postcode" value="<?php echo $password; ?>">
+        <input type="hidden" name="mail" value="<?php echo $mail; ?>">
+        <input type="hidden" name="password" value="<?php echo $password; ?>">
 
         <button type="submit">確定</button>
     </form>
     <form action="customer-input.php" method="post">
         <!-- 戻るボタンで再入力 -->
-        <input type="hidden" name="user_name1" value="<?php echo $user_name1; ?>">
-        <input type="hidden" name="user_name2" value="<?php echo $user_name2; ?>">
-        <input type="hidden" name="postcode" value="<?php echo $postcode; ?>">
+        <input type="hidden" name="name" value="<?php echo $name; ?>">
+        <input type="hidden" name="kana" value="<?php echo $kana; ?>">
+        <input type="hidden" name="post_code" value="<?php echo $post_code; ?>">
         <input type="hidden" name="address" value="<?php echo $address; ?>">
-        <input type="hidden" name="postcode" value="<?php echo $postcode; ?>">
-        <input type="hidden" name="postcode" value="<?php echo $user_mail; ?>">
-        <input type="hidden" name="postcode" value="<?php echo $password; ?>">
+        <input type="hidden" name="mail" value="<?php echo $mail; ?>">
+        <input type="hidden" name="password" value="<?php echo $password; ?>">
         <button type="submit">修正する</button>
     </form>
 </body>
--- login-input.php
+++ login-input.php
@@ -1,5 +1,5 @@
 <form action="login-complete.php" method="post">
-ログイン名<input type="text" name="user_mail"><br>
+メールアドレス<input type="text" name="mail"><br>
 パスワード<input type="password" name="password"><br>
 <input type="submit" value="ログイン">
 </form>
