c9ae0f116ec906e9954949c4b5c3e3634f06887e
takeshitatomoyuki <takeshitaprogramming@gmail.com>
2025-04-01 15:04:19 +0900

    ヘッダー

--- login-input.php
+++ login-input.php
@@ -1,4 +1,4 @@
-<?php require '../header.php'; ?>
+<?php require 'includes/header.php'; ?>
 <h1>ログイン</h1>
 <form action="login-complete.php" method="post">
 メールアドレス<input type="text" name="mail"><br>
