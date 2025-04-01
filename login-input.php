2a5a3017c0425482de4566e765c7321d6edb8eeb
takeshitatomoyuki <takeshitaprogramming@gmail.com>
2025-04-01 14:56:58 +0900

    ヘッダーフッター追加

--- login-input.php
+++ login-input.php
@@ -1,3 +1,4 @@
+<?php require '../header.php'; ?>
 <h1>ログイン</h1>
 <form action="login-complete.php" method="post">
 メールアドレス<input type="text" name="mail"><br>
@@ -5,3 +6,4 @@
 <input type="submit" value="ログインする">
 </form>
 <a href="customer-input">会員登録がお済みでない方はこちら</a>
+<?php require '../footer.php'; ?>
