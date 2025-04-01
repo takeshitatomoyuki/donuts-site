10d92b669aa916a8ad4ac55f208b28dbaf922333
8000-16000 <goto8303taizo@gmail.com>
2025-04-01 11:19:25 +0900

    フッター(｀・ω・´)

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="jp">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,29 +8,41 @@
   <link rel="stylesheet" href="../common/css/reset.css">
   <link rel="stylesheet" href="../common/css/common.css">
 </head>
+
 <body>
 
   <footer>
-    <ul>
-      <li class="nav_text">よくある質問</li>
-      <li class="nav_text">お問い合わせ</li>
-      <li class="nav_text">当サイトのポリシー</li>
-    </ul>
-    <ul>
+    <div class="link_area">
+      <ul class="footer_link">
+        <a href="">
+          <li class="nav_text">よくある質問</li>
+        </a>
+        <a href="">
+          <li class="nav_text">お問い合わせ</li>
+        </a>
+        <a href="">
+          <li class="nav_text">当サイトのポリシー</li>
+        </a>
+      </ul>
+    </div>
+    <ul class="footer_sns">
       <a href="">
-        <li><img src="../common/images/instagram.png" alt=""></li>
+        <li><img src="../common/images/instagram.png" alt="インスタグラムアイコン"></li>
       </a>
       <a href="">
-        <li><img src="../common/images/facebook.png" alt=""></li>
+        <li><img src="../common/images/facebook.png" alt="フェイスブックアイコン"></li>
       </a>
       <a href="">
-        <li><img src="../common/images/x.png" alt=""></li>
+        <li><img src="../common/images/x.png" alt="ツイッターアイコン" class="x_anime"></li>
       </a>
     </ul>
 
-    <img src="../common/images/footer-logo.svg" alt="" class="footer_logo">
+    <div class="logo_area">
+      <img src="../common/images/footer-logo.svg" alt="" class="footer_logo">
+    </div>
     <p>Copyright (C) c.c.donuts All Rights Reserved</p>
   </footer>
-  
+
 </body>
+
 </html>
