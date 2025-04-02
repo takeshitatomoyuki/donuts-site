b34b23fa5520ece8c9710bf48f628179234e1245
takeshitatomoyuki <takeshitaprogramming@gmail.com>
2025-04-02 13:41:54 +0900

    detail

--- detail.php
+++ detail.php
@@ -55,7 +55,7 @@
     <ol class="breadcrumb">
       <li><a href="index.php">TOP</a></li>
       <li id="category"><a href="product.php">商品一覧</a></li>
-      <li id="product">商品名</li>
+      <li id="product"><a href="product.php">商品名</a></li>
     </ol>
   </nav>
   <?php
