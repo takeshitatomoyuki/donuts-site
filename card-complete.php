ab2910ec95704a3220c160338fa2af6e84a85008
Kawagoe <k09160908@yahoo.co.jp>
2025-04-02 11:29:13 +0900

    カードに変更

--- card-complete.php
+++ card-complete.php
@@ -1,24 +1,18 @@
 <?php
 $pdo=new PDO('mysql:host=localhost;dbname=donuts;charset=utf8',
 'staff','password');
-if(isset($_SESSION['customer'])) {
-  $id=$_SESSION['customer']['id'];
-  $sql=$pdo->prepare('select * from customer where id!=? and mail=?');
-  $sql->execute([$id, $_REQUEST['mail']]);
+if(isset($_SESSION['card'])) {
+  $id=$_SESSION['card']['id'];
+  $sql=$pdo->prepare('select * from card where id!=? and card_name=?');
+  $sql->execute([$id, $_REQUEST['card_name']]);
 } else {
-  $sql=$pdo->prepare('select * from customer where mail=?');
-  $sql->execute([$_REQUEST['mail']]);
+  $sql=$pdo->prepare('select * from card where card_name=?');
+  $sql->execute([$_REQUEST['card_name']]);
 }
-<<<<<<< HEAD
-?>
-<<<<<<< HEAD
-<!-- 変更前 -->
-=======
->>>>>>> origin/main
-=======
+
 if(empty($sql->fetchAll())) {
-  if(isset($_REQUEST['customer'])) {
-    $sql=$pdo->prepare('update customer set card_name=?,card_type=?,card_no=?,card_month=?,card_year=?,card_security_code=? where id=?');
+  if(isset($_REQUEST['card'])) {
+    $sql=$pdo->prepare('update card set card_name=?,card_type=?,card_no=?,card_month=?,card_year=?,card_security_code=? where id=?');
     $sql->execute([
       $_REQUEST['card_name'],
       $_REQUEST['card_type'],
@@ -27,7 +21,7 @@ if(empty($sql->fetchAll())) {
       $_REQUEST['card_year'],
       $_REQUEST['card_security_code'],
       $id]);
-    $_SESSION['customer']=[
+    $_SESSION['card']=[
       'id'=>$id,
       'card_name'=>$_REQUEST['card_name'],
       'card_type'=>$_REQUEST['card_type'],
@@ -37,7 +31,7 @@ if(empty($sql->fetchAll())) {
       'card_security_code'=>$_REQUEST['card_security_code']];
     echo 'お客様情報を更新しました。';
   } else {
-    $sql=$pdo->prepare('insert into customer values(null,?,?,?,?,?,?)');
+    $sql=$pdo->prepare('insert into card values(null,?,?,?,?,?,?)');
     $sql->execute([
       $_REQUEST['card_name'],
       $_REQUEST['card_type'],
@@ -49,11 +43,11 @@ if(empty($sql->fetchAll())) {
       '<h2>カード情報登録完了</h2>
       <div>
         <p>クレジットカード情報を登録しました。</p>。
-        <a href="login-input.php">購入手続きを続ける</a>
+        <a href="product.php">購入手続きを続ける</a>
       </div>';
   }
 } else {
-  echo 'ログイン名がすでに使用されていますので、変更してください。';
+  echo 'カード名がすでに使用されていますので、変更してください。';
 }
 ?>
->>>>>>> origin/main
+
