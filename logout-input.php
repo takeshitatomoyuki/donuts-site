9b85ef8bd1c922a46b504f58913632255e137f2d
takeshitatomoyuki <takeshitaprogramming@gmail.com>
2025-04-02 13:20:33 +0900

    ようこそ

--- logout-input.php
+++ logout-input.php
@@ -1,4 +1,12 @@
 <?php require 'includes/header.php'; ?>
+<?php
+session_start(); // セッションを開始
+if (isset($_SESSION['customer'])) {
+    echo 'ようこそ ', $_SESSION['customer']['name'], 'さん。';
+} else{
+  echo 'ようこそ ゲストさん。';
+}
+?>
 <h1>ログアウト</h1>
 <p>ログアウトしますか？</p>
 <a href="logout-complete.php">ログアウトする</a>
