41643d52a38be23d44431b8685bf1e1be82110b2
takeshitatomoyuki <takeshitaprogramming@gmail.com>
2025-04-02 14:08:47 +0900

    login

--- login-input.php
+++ login-input.php
@@ -9,6 +9,16 @@ if (isset($_SESSION['customer'])) {
   echo 'ようこそ ゲストさん。';
 }
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="common/css/reset.css">
+  <link rel="stylesheet" href="common/css/common.css">
+</head>
+<body>
 <h1>ログイン</h1>
 <form action="login-complete.php" method="post">
 メールアドレス<input type="text" name="mail"><br>
@@ -16,4 +26,6 @@ if (isset($_SESSION['customer'])) {
 <input type="submit" value="ログインする">
 </form>
 <a href="customer-input.php">会員登録がお済みでない方はこちら</a>
+</body>
+</html>
 <?php require 'includes/footer.php'; ?>
