55b45a5f5eba0dd93d057219746ec258dc4875a2
8000-16000 <goto8303taizo@gmail.com>
2025-04-03 15:09:51 +0900

    ログイン画面更新

--- login-input.php
+++ login-input.php
@@ -1,9 +1,21 @@
 <?php require 'includes/header.php'; ?>
-<h1>ログイン</h1>
-<form action="login-complete.php" method="post">
-メールアドレス<input type="text" name="mail"><br>
-パスワード<input type="password" name="password"><br>
-<input type="submit" value="ログインする">
-</form>
-<a href="customer-input.php">会員登録がお済みでない方はこちら</a>
+
+<main>
+
+<section class="login_sec">
+  <h1 class="login_title">ログイン</h1>
+  <form action="login-complete.php" method="post" class="login_form">
+  <div class="login_text_block">
+    <div class="login_block">
+      <span class="login_input_text">メールアドレス</span><br><input type="text" name="mail" class="login_mail"><br>
+      <span class="login_input_text">パスワード</span><br><input type="password" name="password" class="login_password"><br>
+    </div>
+  </div>
+  <input type="submit" value="ログインする" class="login_btn">
+  </form>
+  <p class="login_link"><a href="customer-input.php">会員登録がお済みでない方はこちら</a></p>
+</section>
+
+</main  >
+
 <?php require 'includes/footer.php'; ?>
