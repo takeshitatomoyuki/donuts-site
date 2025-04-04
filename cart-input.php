e2b4dfd6a92fed5fea1562f8fef29d1e485f5658
takeshitatomoyuki <takeshitaprogramming@gmail.com>
2025-04-04 13:17:20 +0900

    cart

--- cart-input.php
+++ cart-input.php
@@ -12,6 +12,17 @@ foreach ($sql as $row) {
 	echo '<p>商品名：', $row['name'], '</p>';
 	echo '<p>価格：', $row['price'], '</p>';
 	echo '<p>個数：<select name="count">';}}
+	for ($i=1; $i<=10; $i++) {
+		echo '<option value="', $i, '">', $i, '</option>';
+	}
+	echo '</select></p>';
+	echo '<input type="hidden" name="id" value="', $row['id'], '">';
+	echo '<input type="hidden" name="name" value="', $row['name'], '">';
+	echo '<input type="hidden" name="price" value="', $row['price'], '">';
+	echo '<p><input type="submit" value="カートに追加"></p>';
+	echo '</form>';
+	
+?>
 	?>
 <?php
 $id=$_REQUEST['id'];
