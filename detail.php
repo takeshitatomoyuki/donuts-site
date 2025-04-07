ffd9154aa60cc3bcfeda4c79f1968cb2561c9e17
takeshitatomoyuki <takeshitaprogramming@gmail.com>
2025-04-07 10:49:55 +0900

    detail

--- detail.php
+++ detail.php
@@ -42,23 +42,8 @@
        <p class="detail_heart_box"><img src="common/images/sp_heart.jpg" alt="ハート" class="detail_heart"></p>
       </div>
       <div class="detail_count_box">
-<<<<<<< HEAD
-      <form action="cart-input.php" method="post">
-      <input type="hidden" name="id" value="', $row['id'], '">
-      <input type="hidden" name="name" value="', $row['name'], '">
-      <input type="hidden" name="price" value="', $row['price'], '"> 
-        <p><input type="text" class="detail_count">
-       <?php
-        for ($i=1; $i<=10; $i++) {
-	echo '<option value="', $i, '">', $i, '</option>';
- 	}
-  ?>
-        個<select name="count"></p>
-        <p><input type="submit" value="カートに入れる" class="detail_submit"></p></form>;
-=======
         <p><input type="text" class="detail_count">個</p>
         <p><input type="submit" value="カートに入れる" class="detail_submit"></p>
->>>>>>> origin/main
       </div>
     </div>
   </div>
