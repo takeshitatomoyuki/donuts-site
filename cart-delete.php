9ff866633067070978a94987f9f5f846eabfa633
takeshitatomoyuki <takeshitaprogramming@gmail.com>
2025-04-08 16:09:30 +0900

    cart

--- cart-delete.php
+++ cart-delete.php
@@ -6,5 +6,7 @@ echo 'カートから商品を削除しました。';
 echo '<hr>';}
 require 'cart.php';
 ?>
+ echo'<a href="purchase-confirm.php">ご購入確認へ進む</a>';
+       echo' <a href="product.php">買い物を続ける</a>';
 
 <?php require 'includes\footer.php'; ?>
--- cart-input.php
+++ cart-input.php
@@ -25,6 +25,8 @@ echo '<p>カートに商品を追加しました。</p>';
 echo '<hr>';
 require 'cart.php';
 ?>
+ echo'<a href="purchase-confirm.php">ご購入確認へ進む</a>';
+       echo' <a href="product.php">買い物を続ける</a>';
 
 
 <?php require 'includes\footer.php'; ?>
