eeef83483163557d632f8b03c4a3c9719d266a51
takeshitatomoyuki <takeshitaprogramming@gmail.com>
2025-04-09 15:16:32 +0900

    カート

--- cart.php
+++ cart.php
@@ -21,11 +21,15 @@ if (isset($_SESSION['customer'])) {
         $total = 0;
 
         foreach ($_SESSION['product'] as $id => $product) {
+            foreach ($sql as $row) {
+                echo '<div class="detail_image_box">','<img src="common/images/sp_', $row['id'],'donuts.jpg" alt="１位画像" class="detail_image">','</div>';
+            }
             echo '<table><tbody><tr><td class="purchase_list_left">商品名</td>';
             echo '<td class="purchase_list_right"><a href="detail.php?id=', $id, '">', $product['name'], '</a></td></tr>';
             echo '<tr><td class="purchase_list_left">数量</td>';
             echo '<td class="purchase_list_right">', $product['count'], '個</td></tr>';
             echo '<tr><td class="purchase_list_left">小計</td>';
+            echo '<tr><td class="purchase_list_left">数量</td>';
             echo '<td class="purchase_list_right">税込 ￥', number_format($product['price'] * $product['count']), '</td>';
             echo '<td class="purchase_list_delete"><a href="cart-delete.php?id=', $id, '">削除</a></td>';
             echo '</tr></tbody></table>';
