e53aa595cc46a87e01952cfa9a9e1ad54f907eea
takeshitatomoyuki <takeshitaprogramming@gmail.com>
2025-04-09 10:23:17 +0900

    カート

--- cart-input.php
+++ cart-input.php
@@ -40,9 +40,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <a href="purchase-confirm.php">ご購入確認へ進む</a>
 <a href="product.php">買い物を続ける</a>
 
-<?php
- echo'<a href="purchase-confirm.php">ご購入確認へ進む</a>';
-       echo' <a href="product.php">買い物を続ける</a>';
-?>
+
 
 <?php require 'includes\footer.php'; ?>
