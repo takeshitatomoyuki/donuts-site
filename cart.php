0b2196c2c5f9054862cfc5193a96304abeb04511
takeshitatomoyuki <takeshitaprogramming@gmail.com>
2025-04-09 12:53:33 +0900

    カート

--- cart.php
+++ cart.php
@@ -21,28 +21,24 @@ if (isset($_SESSION['customer'])) {
     // ✅ カート情報もログイン時のみ表示
     if (!empty($_SESSION['product'])) {
         echo '<table>';
-        echo '<tbody>';
-        // echo '<tr><th>商品番号</th><th>商品名</th>';
+echo '<tbody>';
+$total = 0;
+foreach ($_SESSION['product'] as $id => $product) {
+    echo '<tr><td class="purchase_list_left">商品名</td><td class="purchase_list_right"><a href="detail.php?id=', $id, '">', $product['name'], '</a></td></tr>';
+    echo '<tr><td class="purchase_list_left">数量</td><td class="purchase_list_right">', $product['count'], '個</td></tr>';
+    echo '<tr>';
+    echo '<td class="purchase_list_left">小計</td>';
+    echo '<td class="purchase_list_right">税込 ￥', $product['price'] * $product['count'], '</td>';
+    echo '<td class="purchase_list_delete"><a href="cart-delete.php?id=', $id, '">削除</a></td>';
+    echo '</tr>';
+    
+    $subtotal = $product['price'] * $product['count'];
+    $total += $subtotal;
+}
 
-        $total = 0;
-        foreach ($_SESSION['product'] as $id => $product) {
-            echo '<tr>';
-            // echo '<td>', $id, '</td>';
-            echo '<tr>','<td class="purchase_list_left">','商品名','</td>','<td class="purchase_list_right"><a href="detail.php?id=', $id, '">', $product['name'], '</a></td>','</tr>';
-            echo '<tr>','<td class="purchase_list_left">','数量','</td>','<td class="purchase_list_right">', $product['count'],'個', '</td>','</tr>';
-            echo '<tr>','<td class="purchase_list_left">','小計','</td>','<td class="purchase_list_right">', '税込 ￥',$product['price'],'</td>';
-            $subtotal = $product['price'] * $product['count'];
-            $total += $subtotal;
-            echo '<td class="purchase_list_delete"><a href="cart-delete.php?id=', $id, '">削除</a></td>';
-            echo '</tr>';
-            echo '</tbody>';
-        echo '</table>';
-        }
-        echo '<table>';
-        echo '<tbody>';
-        echo '<tr><td class="purchase_list_total">合計</td><td class="purchase_list_right">', '税込 ￥',$total, '</td><td></td></tr>';
-        echo '</tbody>';
-        echo '</table>';
+echo '<tr><td class="purchase_list_total">合計</td><td class="purchase_list_right">税込 ￥', $total, '</td><td></td></tr>';
+echo '</tbody>';
+echo '</table>';
        
     } else {
         echo 'カートに商品がありません。';
