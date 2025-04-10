80bd54004bd093e1f7c416abfbada65eb9f060f8
takeshitatomoyuki <takeshitaprogramming@gmail.com>
2025-04-10 15:51:17 +0900

    サーバー

--- login-complete.php
+++ login-complete.php
@@ -4,6 +4,7 @@
 unset($_SESSION['customer']);
 //$pdo=new PDO('mysql:host=localhost;dbname=donuts;charset=utf8', 'staff', 'password');
 	//$pdo=new PDO('mysql:host=localhost;dbname=donuts;charset=utf8', 'staff', 'password');
+	
 	$pdo = new PDO('mysql:host=localhost;dbname=ss896700_donuts;charset=utf8', 'ss896700_cca', 'ccadonuts');
 
 
