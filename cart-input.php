22bcff9bf1720551e36a8a5e0a6313fbff5f0295
Kawagoe <k09160908@yahoo.co.jp>
2025-04-10 09:01:34 +0900

    全体的

--- cart-input.php
+++ cart-input.php
@@ -4,6 +4,7 @@
 <head>
   <link rel="stylesheet" href="common/css/breadcrumb.css">
 	<link rel="stylesheet" href="common/css/customer-name.css">
+    <link rel="stylesheet" href="common/css/cart-input.css">
   <script src="common/js/breadcrumb.js"></script>
 </head>
 <?php
@@ -33,17 +34,55 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 }
 ?>
 
-<?php
-// ✅ ここを if文で囲んで表示制御！
+<main>
+    <?php
+    // ✅ ここを if文で囲んで表示制御！
+    
+    if (isset($_GET['added'])) {
+        echo '<p class="cart_text">カートに商品を追加しました。</p>';
+    
+    
+    
+    
+        if (isset($_SESSION['customer'])) {
+            if (!empty($_SESSION['product'])) {
+    
+                $total = 0;
+    
+                foreach ($_SESSION['product'] as $id => $product) {
+                    echo '<div class=cart_inner>';
+                    echo '<div class="cart_image_box">';
+                     echo '<img src="common/images/sp_' . $id . 'donuts.jpg" alt="商品画像" class="cart_image">';
+                    echo '</div>';
+                    echo '<div class="cart_inner_right"><p class="cart_product_name"><a href="detail.php?id=', $id, '">', $product['name'], '</a></p>';
+                    echo '<div class="cart_count"><p class="cart_price">税込 ￥', number_format($product['price'] * $product['count']), '</p>';
+                    echo '<p class="cart_number">数量', '<span class="cart_number_span">',$product['count'], ' 個</span></p></div>';
+                    echo '<p class="cart_delete_box"><a class="cart_delete" href="cart-delete.php?id=', $id, '">削除する</a></p></div>';
+                    echo '</div>';
+    
+                    $subtotal = $product['price'] * $product['count'];
+                    $total += $subtotal;
+                }
+    
 
-if (isset($_GET['added'])) {
-    echo '<p>カートに商品を追加しました。</p>';
-    echo '<hr>';
-    require 'cart.php';
-    echo '<a href="purchase-confirm.php">ご購入確認へ進む</a>';
-    echo '<a href="product.php">買い物を続ける</a>';
-}
-?>
+                echo '<div class="cart_total_box"><p class="cart_total">ご注文合計：<span class="cart_total_span">税込￥', number_format($total),'</span></p>';
+                echo '<a class="cart_submit" href="purchase-confirm.php">ご購入確認へ進む</a></div>';
+
+            } else {
+                echo 'カートに商品がありません。';
+            }
+        } else {
+            header("Location: login-input.php");
+            exit();
+        }
+    
+    
+    
+        // echo '<a href="purchase-confirm.php">ご購入確認へ進む</a>';
+        echo '<p><a class="cart_submit1" href="product.php">買い物を続ける</a></p>';
+    }
+    ?>
+</main>
 
 
 <?php require 'includes\footer.php'; ?>
--- cart.php
+++ cart.php
@@ -1,15 +1,4 @@
-<!-- <!DOCTYPE html>
-<html lang="jp">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="common/css/reset.css">
-  <link rel="stylesheet" href="common/css/customer-confirm.css">
-</head>
-
-<body> -->
 <?php
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
@@ -22,16 +11,16 @@ if (isset($_SESSION['customer'])) {
 
         foreach ($_SESSION['product'] as $id => $product) {
             echo '<div class="detail_image_box">';
-echo '<img src="common/images/sp_' . $id . 'donuts.jpg" alt="商品画像" class="detail_image">';
-echo '</div>';
+// echo '<img src="common/images/sp_' . $id . 'donuts.jpg" alt="商品画像" class="detail_image">';
+// echo '</div>';
             echo '<table><tbody><tr><td class="purchase_list_left">商品名</td>';
             echo '<td class="purchase_list_right"><a href="detail.php?id=', $id, '">', $product['name'], '</a></td></tr>';
             echo '<tr><td class="purchase_list_left">数量</td>';
             echo '<td class="purchase_list_right">', $product['count'], '個</td></tr>';
             echo '<tr><td class="purchase_list_left">小計</td>';
             echo '<td class="purchase_list_right">税込 ￥', number_format($product['price'] * $product['count']), '</td>';
-            echo '<tr><td class="purchase_list_left">数量</td>';
-            echo '<td class="purchase_list_right">', $product['count'], '個</td></tr>';
+            // echo '<tr class="cart_input_list"><td>数量</td>';
+            // echo '<td>', $product['count'], '個</td></tr>';
             echo '<td class="purchase_list_delete"><a href="cart-delete.php?id=', $id, '">削除</a></td>';
             echo '</tr></tbody></table>';
 
